60690dc687439495ddcf802afe4d4b6bf1e58c33
terrill-chalmers <terrill-chalmers@protonmail.com>
2021-11-24 13:11:32 -0600

    init commit

---
+++ photo-share/src/main/java/dev/chalmers/photoshare/PhotoShareApplication.java
@@ -0,0 +1,13 @@
+package dev.chalmers.photoshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoShareApplication.class, args);
+	}
+
+}
